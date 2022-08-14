64bac04307de7981166f2fbcc436910ee1c70d86
OffsetMonkey539 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-08-14 20:24:50 +0200

    Add VillagerEntity mixin

---
+++ src/main/java/io/github/offsetmonkey538/villagertaming/mixin/VillagerEntityMixin.java
@@ -0,0 +1,8 @@
+package io.github.offsetmonkey538.villagertaming.mixin;
+
+import net.minecraft.entity.passive.VillagerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(VillagerEntity.class)
+public abstract class VillagerEntityMixin {
+}
