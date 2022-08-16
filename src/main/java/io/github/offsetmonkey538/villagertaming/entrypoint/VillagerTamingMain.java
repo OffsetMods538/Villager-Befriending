337619d7afc8fdbec023c376de152f0a4fc5f0dd
OffsetMonkey539 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-08-16 14:38:02 +0200

    Add MOD_ID variable

--- src/main/java/io/github/offsetmonkey538/villagertaming/entrypoint/VillagerTamingMain.java
+++ src/main/java/io/github/offsetmonkey538/villagertaming/entrypoint/VillagerTamingMain.java
@@ -1,12 +1,12 @@
 package io.github.offsetmonkey538.villagertaming.entrypoint;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.passive.VillagerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VillagerTamingMain implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("villagertaming");
+	public static final String MOD_ID = "villagertaming";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
