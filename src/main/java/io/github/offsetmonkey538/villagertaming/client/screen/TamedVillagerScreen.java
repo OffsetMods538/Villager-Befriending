5ac7c5a2eb58f99126b341e593a781ac8a98dc66
OffsetMonkey539 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-08-17 12:39:09 +0200

    Add test buttons to screen

--- src/main/java/io/github/offsetmonkey538/villagertaming/client/screen/TamedVillagerScreen.java
+++ src/main/java/io/github/offsetmonkey538/villagertaming/client/screen/TamedVillagerScreen.java
@@ -1,12 +1,26 @@
 package io.github.offsetmonkey538.villagertaming.client.screen;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.offsetmonkey538.villagertaming.screen.TamedVillagerScreenHandler;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import static io.github.offsetmonkey538.villagertaming.entrypoint.VillagerTamingMain.MOD_ID;
+import static io.github.offsetmonkey538.villagertaming.packet.ModPackets.TAMED_VILLAGER_MENU_BUTTON_PRESSED;
 
 public class TamedVillagerScreen extends HandledScreen<TamedVillagerScreenHandler> {
+    private static final Identifier TEXTURE_PATH = new Identifier(MOD_ID, "textures/gui/tamed_villager.png");
+
+    public static final int TEST_BUTTON_ID = 1;
+    public static final int TEST_BUTTON_2_ID = 2;
 
     public TamedVillagerScreen(TamedVillagerScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -14,6 +28,36 @@ public class TamedVillagerScreen extends HandledScreen<TamedVillagerScreenHandle
 
     @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, TEXTURE_PATH);
+        int x = (width - backgroundWidth) / 2;
+        int y = (height - backgroundHeight) / 2;
+        drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
+    }
 
+    @Override
+    protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
+        this.textRenderer.draw(matrices, this.title, (float)this.titleX, (float)this.titleY, 0x404040);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
+
+        addDrawableChild(new ButtonWidget(width / 2, height / 2, 20, 20, Text.of("Test"), button ->
+            sendButtonPressedPacket(TEST_BUTTON_ID)
+        ));
+        addDrawableChild(new ButtonWidget(x + (backgroundWidth / 2 - 20), y + 20, 40, 20, Text.of("Test 2"), button ->
+            sendButtonPressedPacket(TEST_BUTTON_2_ID)
+        ));
+    }
+
+    private void sendButtonPressedPacket(int buttonId) {
+        PacketByteBuf byteBuffer = PacketByteBufs.create();
+        byteBuffer.writeInt(buttonId);
+
+        ClientPlayNetworking.send(TAMED_VILLAGER_MENU_BUTTON_PRESSED, byteBuffer);
     }
 }
--- src/main/java/io/github/offsetmonkey538/villagertaming/screen/TamedVillagerScreenHandler.java
+++ src/main/java/io/github/offsetmonkey538/villagertaming/screen/TamedVillagerScreenHandler.java
@@ -1,14 +1,35 @@
 package io.github.offsetmonkey538.villagertaming.screen;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 
+import static io.github.offsetmonkey538.villagertaming.client.screen.TamedVillagerScreen.TEST_BUTTON_2_ID;
+import static io.github.offsetmonkey538.villagertaming.client.screen.TamedVillagerScreen.TEST_BUTTON_ID;
+import static io.github.offsetmonkey538.villagertaming.packet.ModPackets.TAMED_VILLAGER_MENU_BUTTON_PRESSED;
+import static io.github.offsetmonkey538.villagertaming.entrypoint.VillagerTamingMain.LOGGER;
+
 public class TamedVillagerScreenHandler extends ScreenHandler {
 
-    public TamedVillagerScreenHandler(int syncId, PlayerInventory playerInventory) {
+    private final Inventory inventory;
+
+    public TamedVillagerScreenHandler(int syncId, PlayerInventory inventory) {
         super(ModScreenHandlers.TAMED_VILLAGER, syncId);
+        this.inventory = inventory;
+
+        ServerPlayNetworking.registerGlobalReceiver(TAMED_VILLAGER_MENU_BUTTON_PRESSED, (server, player, handler, buf, responseSender) -> {
+            int buttonId = buf.readInt();
+            server.execute(() -> {
+                switch (buttonId) {
+                    case TEST_BUTTON_ID -> LOGGER.info("First button has been pressed by {}", player.getDisplayName());
+                    case TEST_BUTTON_2_ID -> LOGGER.info("Second button has been pressed {}", player.getDisplayName());
+                    default -> LOGGER.warn("Button with id {} not found!", buttonId);
+                }
+            });
+        });
     }
 
     @Override
@@ -18,6 +39,6 @@ public class TamedVillagerScreenHandler extends ScreenHandler {
 
     @Override
     public boolean canUse(PlayerEntity player) {
-        return false;
+        return inventory.canPlayerUse(player);
     }
 }
