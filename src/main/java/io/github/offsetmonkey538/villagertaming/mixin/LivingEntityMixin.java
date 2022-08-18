5031a8fdbe25dc7e4a6b758f0e236b01cc8c2ba6
OffsetMonkey539 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-08-18 22:07:52 +0200

    Add checks for 'standing', 'followingOwner', and 'wandering' inside of the 'LivingEntity#isImmobile' mixin.

--- src/main/java/io/github/offsetmonkey538/villagertaming/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/offsetmonkey538/villagertaming/mixin/LivingEntityMixin.java
@@ -2,7 +2,6 @@ package io.github.offsetmonkey538.villagertaming.mixin;
 
 import io.github.offsetmonkey538.villagertaming.entity.IVillagerData;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.VillagerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,10 +15,9 @@ public abstract class LivingEntityMixin {
         at = @At("TAIL"),
         cancellable = true
     )
-    @SuppressWarnings("ConstantConditions")
     public void villagertaming$makeStandingVillagersImmobile(CallbackInfoReturnable<Boolean> cir) {
-        if ((Object)this instanceof VillagerEntity villagerEntity) {
-            if (((IVillagerData)villagerEntity).isStanding()) {
+        if (this instanceof IVillagerData villagerData) {
+            if (villagerData.isStanding() && !(villagerData.isFollowingOwner() || villagerData.isWandering())) {
                 cir.setReturnValue(true);
             }
         }
