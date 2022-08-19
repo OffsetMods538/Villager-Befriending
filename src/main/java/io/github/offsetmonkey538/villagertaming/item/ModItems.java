ccc7e9b8e9c493e3e5d36313a4f1e48461a340e8
OffsetMonkey539 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-08-19 11:42:49 +0200

    Change taming item rarity, now the name is :sparkles:colorful:sparkles:

--- src/main/java/io/github/offsetmonkey538/villagertaming/item/ModItems.java
+++ src/main/java/io/github/offsetmonkey538/villagertaming/item/ModItems.java
@@ -4,13 +4,14 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 import static io.github.offsetmonkey538.villagertaming.entrypoint.VillagerTamingMain.MOD_ID;
 
 public class ModItems {
 
-    public static final TotameItem TOTAME_ITEM = register(new TotameItem(new FabricItemSettings().group(ItemGroup.MISC)), "totame");
+    public static final TotameItem TOTAME_ITEM = register(new TotameItem(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.RARE)), "totame");
 
     private static <T extends Item> T register(T item, String id) {
         return Registry.register(Registry.ITEM, new Identifier(MOD_ID, id), item);
